e832aa7c532f4d55f5c6eb23a428d91c4720d9f3
cmdax <cmdax@proton.me>
2024-05-13 19:50:28 +0200

    Make it pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
+    let baseArray = arguments[0];
 
+    for (let indexArg = 1; indexArg < arguments.length; indexArg++) {
+        let elementToCheck = arguments[indexArg];
+        
+        for (let indexBase = 0; indexBase < baseArray.length; indexBase++){
+            let elementInBase = baseArray[indexBase];
+
+            if (elementToCheck === elementInBase) {
+                baseArray.splice(indexBase,1);
+                indexBase--;
+            }
+        }
+    }
+
+    return baseArray;
 };
 
 // Do not edit below this line
