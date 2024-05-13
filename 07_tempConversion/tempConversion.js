53c9cb30a9f1d81acb537614de08468c51ea7514
cmdax <cmdax@proton.me>
2024-05-13 20:21:00 +0200

    Make it pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+// Celsius is 5/9 of (Fahrenheit - 32)
+const convertToCelsius = function(tempF) {
+  return Math.round((5/9 * (tempF - 32)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// Fahrenheit is 9/5 of Celsius + 32
+const convertToFahrenheit = function(tempC) {
+  return Math.round((9/5 * tempC + 32) * 10) / 10;
 };
 
 // Do not edit below this line
