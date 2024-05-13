9f412e681b3640abeefa47c20da85003ad4065a8
cmdax <cmdax@proton.me>
2024-05-13 17:00:46 +0200

    Make it pass all test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, nbOfRepetition) {
+    if (nbOfRepetition < 0) {
+        return 'ERROR';
+    }
 
+    if (word === '') {
+        return '';
+    }
+
+    let finalString = '';
+
+    for (i = 0; i < nbOfRepetition; i++) {
+        finalString += word;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
