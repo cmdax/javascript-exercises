50035f000022b2a28d08e0a6a7d41aa6cf27d2f7
cmdax <cmdax@proton.me>
2024-05-13 20:10:58 +0200

    Make it pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divisibleBy4 = (year % 4 == 0);
+    let divisibleBy100 = (year % 100 == 0);
+    let divisibleBy400 = (year % 400 == 0);
 
+    if (divisibleBy4) {
+        if (divisibleBy100 && divisibleBy400) {
+            return true;
+        } else if (divisibleBy100) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
