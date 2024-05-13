77b2a82994dc9076f745d5aa7685805d18397a23
cmdax <cmdax@proton.me>
2024-05-13 20:02:31 +0200

    Make it pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if ((typeof firstNumber != 'number' || typeof secondNumber != 'number' 
+    || firstNumber < 0 || secondNumber < 0 )) {return 'ERROR'}
+
+    if (firstNumber > secondNumber) {
+        [firstNumber, secondNumber] = [secondNumber, firstNumber];
+    }
+    
+    let sum = 0;
+
+    for (number = firstNumber; number <= secondNumber; number++){
+        sum += number;
+    }
+
+    return sum;
 
 };
 
